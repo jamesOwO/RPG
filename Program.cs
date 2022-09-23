8f56c8ae3b94cff0b0c350b6a550ec499188042d
josh <537387@chrome.escg.ac.uk>
2022-09-23 09:09:27 +0100

    started coding my RPG

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Welcome to... \nWhat is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}\nPress enter to begin", name);
+            Console.ReadLine();
+            Console.Clear();
+            Thread.Sleep(2000);
+            Console.WriteLine("BAanng");
+            Thread.Sleep(2000);
+        }
+    }
+}
