14a0c3fb473dcddc842d05fa65e0c71ad19f7688
josh <537387@chrome.escg.ac.uk>
2022-09-23 22:13:07 +0100

    begins next morning

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             string name;
-
-            //variables
-            string squirrel
+            string squirrel;
+            int squirrelmaxhp = 100;
+            int squirrelhp = 100;
+            
 
             Console.WriteLine("Welcome to... \nWhat is your name?");
             name = Console.ReadLine();
@@ -19,11 +20,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Thread.Sleep(2000);
             Console.WriteLine("You look up from the ground \nYou're laying on a pile of boxes in a familiar looking alleyway with what looks like your squirrel companion " +
                 "asleep on your lap.\nWhat is their name?");
-            squirrel = Console.readline();
+            squirrel = Console.ReadLine();
             Console.WriteLine("Your friend James runs up to you\nJames - 'OMG, i heard what happened are you ok? What are you going to do about the people that took over " +
                 "your animal fighting business?'\nYou - 'I will have to get it back then'\nJames takes you and {0} back to his appartment till the morning", squirrel);
+            Thread.Sleep(10000);
             Console.Clear();
-            Co
+            Console.WriteLine("In the morning you notice theres a note from James on the table which reads 'Hey {0}, I need to go out of the city for a while, you can use my " +
+                "appartment for as long as you need'", name);
+
+            
+
+            
         }
     }
 }
