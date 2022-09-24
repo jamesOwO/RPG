83f0407389da8d540b6a8cb85d5d6ffdcded7a3a
josh <537387@chrome.escg.ac.uk>
2022-09-25 00:34:39 +0100

    began adding the combat

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,11 +8,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            Random ranumber = new Random();
             string name;
             string squirrel;
             int squirrelmaxhp = 100;
+            int enemyhp = 100;
+            string enemy = "cat";
             int squirrelhp = 100;
-            
+            string move;
+            string secondarymove;
+            bool myblock = false;
+            bool squirrelbleed = false;
+            bool enemyblock = false;
+            bool enemybleed = false;
+            Double damage = 1;
+            Double defence = 1;
+            Double speed = 1;
+            bool winner = false;
 
             Console.WriteLine("Welcome to... \nWhat is your name?");
             name = Console.ReadLine();
@@ -28,7 +42,67 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("In the morning you notice theres a note from James on the table which reads 'Hey {0}, I need to go out of the city for a while, you can use my " +
                 "appartment for as long as you need'", name);
 
-            
+             
+            while (winner == false) {
+                Console.WriteLine("What do you want {0} to do? \n 1 - attack \n 2 - special \n 3 - distract \n 4 - use item");
+                move = Console.ReadLine();
+                switch (move) {
+                    case "1" || "attack":
+                        enemyhp = enemyhp - 20;
+                        Console.WriteLine("you dealt 20 damage to {0}", enemy);
+                    case "2" || "special":
+                        enemyhp = enemyhp - 5;
+                        enemybleed = true;
+                        Console.WriteLine("You dealt 5 damage to {0} \n {0} has started to bleed", enemy);
+                    case "3" || "distract":
+                        myblock = true;
+                        Console.WriteLine("You used a teddy to replace your animal");
+                    case "4" || "use item":
+                        Console.WriteLine("What item would you like to use? \n 1 - medkit \n 2 - bandage \n 3 - drugs");
+                        secondarymove = Console.ReadLine();
+                        if (secondarymove == "1")
+                        {
+                            squirrelhp = squirrelhp + 50;
+                            if (squirrelhp > squirrelmaxhp)
+                            {
+                                squirrelhp = 100;
+                            }
+                            Console.WriteLine("{0}'s hp was restored to {1} health", squirrel, squirrelhp);
+                        }
+                        else if (secondarymove == "2")
+                        {
+                            if (squirrelbleed == true)
+                            {
+                                squirrelbleed = false;
+                                Console.WriteLine("{0} stopped bleeding");
+                            }
+                            else
+                            {
+                                Console.WriteLine("it did nothing");
+                            }
+                        }
+                        else if (secondarymove == "3")
+                        {
+                            int ranumber = rnd.Next(1, 3);
+                            if (ranumber == 1)
+                            {
+                                damage = damage + 1.5;
+                                Console.WriteLine("{0}'s damage was increased", squirrel);
+                            }
+                            else if (ranumber == 2)
+                            {
+                                defence = defence + 1.5;
+                                Console.WriteLine("{0}'s defence was increased", squirrel);
+                            }
+                            else if (ranumber == 3)
+                            {
+                                speed = speed + 1.5;
+                                Console.WriteLine("{0}'s speed was increased", squirrel);
+                            }
+
+
+                        }
+            }
 
             
         }
